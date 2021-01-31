854eed5c5839ec02d8356334c6712289d183d83e
Abdirahman0022 <76562038+Abdirahman0022@users.noreply.github.com>
2021-01-31 20:44:40 +0300

    Create datatype.js

---
+++ datatype.js
@@ -0,0 +1,21 @@
+// DTA Types 
+// String
+var magac = "Abdirahman";
+
+//Numeric Data type
+var a = 25;         // integer
+var b = 80.5;       // floating-point number
+var c = 4.25e+6;    // exponential notation, same as 4.25e6 or 4250000
+var d = 4.25e-6;    // exponential notation, same as 0.00000425
+
+// Object Data Types
+// null is data type but javascript knows as object {its bug}
+var ma_xariifbaa = null;
+var ma_cada = undefined; // undefined 
+var arr  = [1,2,3,4]; // Array 
+
+//boolean Data Types
+var isReading = true;   // yes, I'm reading
+var isSleeping = false; // no, I'm not sleeping
+
+typeof magac;
